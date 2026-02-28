df1b08070987462c17c3321403ba7c8a2f15ba21
zainabad <zainabad27@gmail.com>
2026-02-28 09:26:48 +0500

    spiral matrix done and dusted.

--- spiralmatrix.cpp
+++ spiralmatrix.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 vector<int> spiralOrder(vector<vector<int>> &matrix)
 {
+    int totalElements = matrix.size() * matrix[0].size();
     vector<int> ans;
     int startingrow = 0;
     int lastrow = matrix.size() - 1;
@@ -14,8 +15,10 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
     int row = 0;
     int col = 0;
 
-    while (startingcol != lastcol && startingrow != lastrow)
+    while (startingcol <= lastcol || startingrow <= lastrow)
     {
+        if (ans.size() == totalElements)
+            return ans;
 
         // loop that runs forward
         while (col <= lastcol)
@@ -23,18 +26,28 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
             ans.push_back(matrix[row][col]);
             col++;
         }
+        col--;
 
         row++;
         startingrow++;
         // loop that runs downwards
+
+        // we have to put some condition here so that if we are in the last iteration our loop breaks here bro.
+        if (ans.size() == totalElements)
+            return ans;
 
         while (row <= lastrow)
         {
             ans.push_back(matrix[row][col]);
             row++;
         }
+        row--;
+
         col--;
         lastcol--;
+
+        if (ans.size() == totalElements)
+            return ans;
 
         // loop that runs backward
         while (col >= startingcol)
@@ -42,16 +55,21 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
             ans.push_back(matrix[row][col]);
             col--;
         }
+        col++;
 
         row--;
         lastrow--;
 
+        if (ans.size() == totalElements)
+            return ans;
         // loop that runs upward
         while (row >= startingrow)
         {
             ans.push_back(matrix[row][col]);
             row--;
         }
+        row++;
+
         col++;
         startingcol++;
     }
@@ -61,7 +79,8 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
 
 int main()
 {
-    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    // vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    vector<vector<int>> matrix = {{1, 2, 3}, {5, 6, 7}, {9, 8, 7}};
 
     vector<int> ans = spiralOrder(matrix);
 
